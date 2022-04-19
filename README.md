2748385c55c480eeffc7e9b7f3d06508023b1d74
thjkral <thjkral@gmail.com>
2022-04-19 22:29:30 +0200

    Updated and renamed multiple files

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Would I have survived the Titanic?
 
-A classification project with passenger data from the most famous shipwreck. 
+A classification project with passenger data from the most famous shipwreck. The project is described in [this](https://tomkral.nl/projects/Titanic/titanic.html) article. I have multiple script that each contain steps for my research. 
+
+### clean_and_EDA.py
+This script has two primary tasks:
+1. **Clean data:** Raw data is taken in and missing values are deleted. This is also explored with a graph.
+2. **Exploratory data analysis:** The data is explored and visualized to gain more insights prior to making a model
+
+This script has several plots and graphs as output. It also makes a new .csv file with the cleaned data.
+
+
+### fit_model_scaler.py
+First, the data is scaled with a min-max-scaler to have all data on the same scale. Second, a suitable __k-value__ is calculated and used to make a k-Nearest Neighbor model. Lastly, both the scaler and model are exported as objects to be loaded later. These can be used in the [webapp](https://github.com/thjkral/titanic_webapp) or in other scrits.
--- python/fit_model_scaler.py
+++ python/fit_model_scaler.py
@@ -10,33 +10,30 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import joblib
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.linear_model import LogisticRegression
 
 # Read data
 df = pd.read_csv('../data/titanic_cleaned.csv', index_col=0)
 
-# Scale the data
-def min_max_scaling(column):
-    return (column - column.min()) / (column.max() - column.min())
-
-df_norm = df
-df_norm['Age'] = min_max_scaling(df_norm['Age'])
-df_norm['SibSp'] = min_max_scaling(df_norm['SibSp'])
-df_norm['Parch'] = min_max_scaling(df_norm['Parch'])
-df_norm['Fare'] = min_max_scaling(df_norm['Fare'])
-
-
 # Split in training and testsets
-x_train, x_test, y_train, y_test = train_test_split(df_norm.drop('Survived', axis=1),
-                                                    df_norm['Survived'],
+x_train, x_test, y_train, y_test = train_test_split(df.drop('Survived', axis=1),
+                                                    df['Survived'],
                                                     test_size=0.30,
                                                     random_state=42)
+
+# Normalize the x data
+scaler = MinMaxScaler()
+scaler.fit(x_train)
+x_train[:] = scaler.fit_transform(x_train)
+x_test[:] = scaler.fit_transform(x_test)
+
+
 
 '''
 K-Nearest Neighbors
@@ -57,7 +54,7 @@ print('Allround score: ', round(knn_model_one.score(x_train, y_train) * 100, 2),
 # Testing multiple k-values
 error_rates = []
 
-for i in range(1,41): # calculate error rates
+for i in range(1,71): # calculate error rates
     knn = KNeighborsClassifier(n_neighbors=i)
     knn.fit(x_train, y_train)
     pred = knn.predict(x_test)
@@ -65,8 +62,8 @@ for i in range(1,41): # calculate error rates
 
 
 plt.figure(figsize=(10,10))
-plt.plot(range(1,41), error_rates, color="blue", marker='o', markerfacecolor='red')
-plt.title('Error Rates vs. K Value', size=20)
+plt.plot(range(1,71), error_rates, color="blue", marker='o', markerfacecolor='red')
+plt.title('Error Rates vs. K-values (k-values 1 - 70)', size=20)
 plt.xlabel('K-value', size=15)
 plt.ylabel('Error rate', size=15)
 #plt.savefig('../plots/knn_errorrates.png')
@@ -74,26 +71,51 @@ plt.ylabel('Error rate', size=15)
 plt.close()
 
 
-# Make new model with k=36
-knn_model_two = KNeighborsClassifier(n_neighbors=36)
+# Make new model with k=63
+knn_model_two = KNeighborsClassifier(n_neighbors=63)
 knn_model_two.fit(x_train, y_train)
 prediction_a = knn_model_two.predict(x_test)
 
 # Evaluate model
-print('\nEVALUATION OF SECOND MODEL (k=36):\n')
+print('\nEVALUATION OF SECOND MODEL (k=63):\n')
 print('Classification report:\n', classification_report(y_test, prediction_a), '\n')
 print('Confusion matrix:\n', confusion_matrix(y_test, prediction_a), '\n')
 print('Allround score: ', round(knn_model_two.score(x_train, y_train) * 100, 2), '\n')
 
+
+'''
+Save the model and scaler to files
+'''
+filename_model = '../model_and_scaler/knn_titanic_model.sav'
+joblib.dump(knn_model_two, filename_model)
+
+filename_scaler = '../model_and_scaler/knn_titanic_scaler.sav'
+joblib.dump(scaler, filename_scaler)
+
+
+
+
+
 '''
 Add new data and predict
 '''
+print(x_train.columns)
+toms_data = {'Pclass':3, 'Age':30, 'SibSp':1, 'Parch':0, 'Fare':14, 'male':1, 'Q':1, 'S':0}
+new_data = np.array([3, 30, 1, 0, 14, 1, 1, 0])
+new_data = new_data.reshape(1, -1)
+new_data = scaler.transform(new_data)
 
-#toms_data = {'Pclass':3, 'Age':30, 'SibSp':1, 'Parch':0, 'Fare':14, 'male':1, 'Q':1, 'S':0}
-tom_array = np.array([3,30,1,0,14,1,1,0])
-tom_array = tom_array.reshape(1, -1)
+new_pred = knn_model_two.predict(new_data)
 
-toms_rediction = knn_model_two.predict(tom_array)
+if new_pred[0] == 0:
+    print('YOU DIED')
+elif new_pred[0] == 1:
+    print('You survived')
+else:
+    print('Something went wrong')
+
+
+    
 
 
 
